165906f4a0d925a7b35d7dd886bf0218fa82f7d4
Matija Karaula <Matija.Karaula@infobip.com>
2022-12-08 20:55:18 +0100

    Initial commit

---
+++ src/main/java/com/infobip/iotthingssimulator/IotthingssimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.infobip.iotthingssimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotthingssimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotthingssimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infobip/iotthingssimulator/IotthingssimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infobip.iotthingssimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotthingssimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
